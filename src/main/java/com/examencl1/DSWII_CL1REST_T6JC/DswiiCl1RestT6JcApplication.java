467e16cbeb798f0c73cd1a417d39d4663f4bf6d6
Bruno-xd <gonzalesramirezbruno@gmail.com>
2023-09-29 20:00:51 -0500

    First commit

---
+++ src/main/java/com/examencl1/DSWII_CL1REST_T6JC/DswiiCl1RestT6JcApplication.java
@@ -0,0 +1,13 @@
+package com.examencl1.DSWII_CL1REST_T6JC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1RestT6JcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1RestT6JcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examencl1/DSWII_CL1REST_T6JC/DswiiCl1RestT6JcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examencl1.DSWII_CL1REST_T6JC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1RestT6JcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
